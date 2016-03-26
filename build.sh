f1be4ef4c34c32d35aa5a6983563e36b18078365
James Ko <jamesqko@gmail.com>
2016-03-25 23:08:27 -0400

    [1/2] Add support for running tests

--- build.sh
+++ build.sh
@@ -11,10 +11,18 @@ build_subdir()
     done
 }
 
+run_tests()
+{
+    find "$1" -type f -name project.json -exec dnx -p {} test \;
+}
+
+# Check prereqs
 type dnu > /dev/null 2>&1 || (echo "dnu isn't in your PATH!" 1>&2 && exit 1)
+type dnx > /dev/null 2>&1 || (echo "dnx isn't in your PATH!" 1>&2 && exit 1)
 
 scriptdir=$(dirname "$0")
 cd -P "$scriptdir"
 
 build_subdir src
 build_subdir test
+run_tests test
