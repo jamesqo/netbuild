81db2bab17c746660a249262ec268af9cf81f4e7
James Ko <jamesqko@gmail.com>
2016-03-25 22:22:08 -0400

    Update build.cmd

---
+++ build.cmd
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+goto main
+
+:: Functions
+
+:buildSubdir
+
+pushd %1
+for /f %%projectFile in ('dir /b /s project.json') do (
+    dnu restore "%%projectFile"
+    dnu build "%%projectFile"
+    dnu pack "%%projectFile"
+)
+popd
+goto :EOF
+
+:main
+
+:: Check for dnu
+where dnu > NUL 2>&1
+if %ERRORLEVEL% NEQ 0 (
+    echo dnu wasn't found in your PATH! 1>&2
+    echo See http://docs.asp.net/en/latest/getting-started/installing-on-windows.html for instructions on installing the DNX toolchain on your PC. 1>&2
+    exit /b %ERRORLEVEL%
+)
+
+:: Do the actual work
+call :buildSubdir src
+call :buildSubdir test
