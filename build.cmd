95eaa8e0e5ade100d60508fe02202b07229b392f
James Ko <jamesqko@gmail.com>
2016-04-09 11:38:58 -0400

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -10,34 +10,42 @@ goto ParseOptions
 
 :BuildSubdir
 
-pushd %1
-for /f "delims=" %%p in ('%ListProjects%') do call dnu pack "%%p" --configuration "%Config%"
-popd
+for %%d in (%*) do (
+    pushd %%d
+    for /f "delims=" %%p in ('%ListProjects%') do call dnu pack "%%p" --configuration "%Config%"
+    popd
+)
 goto :EOF
 
 :CheckInstalled
 
-where %1 > NUL 2>&1
-if %ERRORLEVEL% NEQ 0 (
-    echo %1 wasn't found in your PATH! 1>&2
-    echo See http://docs.asp.net/en/latest/getting-started/installing-on-windows.html for instructions on installing the DNX toolchain on your PC. 1>&2
-    exit /b %ERRORLEVEL%
+for %%p in (%*) do (
+    where %%p > NUL 2>&1
+    if %ERRORLEVEL% NEQ 0 (
+        echo %%p wasn't found in your PATH! 1>&2
+        echo See http://docs.asp.net/en/latest/getting-started/installing-on-windows.html for instructions on installing the DNX toolchain on your PC. 1>&2
+        exit /b %ERRORLEVEL%
+    )
 )
 goto :EOF
 
 :Restore
 
-pushd %1
-:: dnu is a batch script, so we need to `call` it
-for /f "delims=" %%p in ('%ListProjects%') do call dnu restore "%%p"
-popd
+for %%d in (%*) do (
+    pushd %%d
+    :: dnu is a batch script, so we need to `call` it
+    for /f "delims=" %%p in ('%ListProjects%') do call dnu restore "%%p"
+    popd
+)
 goto :EOF
 
 :RunTests
 
-pushd %1
-for /f "delims=" %%p in ('%ListProjects%') do dnx -p "%%p" test
-popd
+for %%d in (%*) do (
+    pushd %%d
+    for /f "delims=" %%p in ('%ListProjects%') do dnx -p "%%p" test
+    popd
+)
 goto :EOF
 
 :: Entry point
@@ -63,12 +71,10 @@ goto ParseOptions
 :ParsingDone
 
 :: Check for dnu and dnx
-call :CheckInstalled dnu
-call :CheckInstalled dnx
+call :CheckInstalled dnu dnx
 
 :: Do the actual work
 cd %~dp0
-call :Restore src
-call :Restore test
+call :Restore src test
 call :BuildSubdir src
 call :RunTests test
