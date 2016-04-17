4052f340ababb17c226719ad144e35dee5b51c6b
James Ko <jamesqko@gmail.com>
2016-04-17 18:25:55 -0400

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -2,6 +2,8 @@
 setlocal EnableDelayedExpansion
 
 set Config=Release
+:: CMD can't capture the output of functions unfortunately,
+:: so we have to make a variable containing the commands we want to run
 set "ListProjects=dir /a-d /b /s project.json ^| findstr /v /i /c:bin /c:obj"
 
 goto ParseOptions
@@ -11,8 +13,8 @@ goto ParseOptions
 :BuildSubdir
 
 for %%d in (%*) do (
-    pushd %%d
-    for /f "delims=" %%p in ('%ListProjects%') do call dnu pack "%%p" --configuration "%Config%"
+    pushd "%%d"
+    for /f "delims=" %%p in ('!ListProjects!') do call dnu pack "%%p" --configuration "!Config!"
     popd
 )
 goto :EOF
@@ -20,11 +22,12 @@ goto :EOF
 :CheckInstalled
 
 for %%p in (%*) do (
-    where %%p > NUL 2>&1
-    if %ERRORLEVEL% NEQ 0 (
-        echo %%p wasn't found in your PATH! 1>&2
+    set "Program=%%p"
+    where "!Program!" > NUL 2>&1
+    if !ERRORLEVEL! NEQ 0 (
+        echo !Program! wasn't found in your PATH! 1>&2
         echo See http://docs.asp.net/en/latest/getting-started/installing-on-windows.html for instructions on installing the DNX toolchain on your PC. 1>&2
-        exit /b %ERRORLEVEL%
+        exit /b !ERRORLEVEL!
     )
 )
 goto :EOF
@@ -32,9 +35,9 @@ goto :EOF
 :Restore
 
 for %%d in (%*) do (
-    pushd %%d
+    pushd "%%d"
     :: dnu is a batch script, so we need to `call` it
-    for /f "delims=" %%p in ('%ListProjects%') do call dnu restore "%%p"
+    for /f "delims=" %%p in ('!ListProjects!') do call dnu restore "%%p"
     popd
 )
 goto :EOF
@@ -42,8 +45,8 @@ goto :EOF
 :RunTests
 
 for %%d in (%*) do (
-    pushd %%d
-    for /f "delims=" %%p in ('%ListProjects%') do dnx -p "%%p" test
+    pushd "%%d"
+    for /f "delims=" %%p in ('!ListProjects!') do dnx -p "%%p" test
     popd
 )
 goto :EOF
@@ -74,7 +77,7 @@ goto ParseOptions
 call :CheckInstalled dnu dnx
 
 :: Do the actual work
-cd %~dp0
+cd "%~dp0"
 call :Restore src test
 call :BuildSubdir src
 call :RunTests test
